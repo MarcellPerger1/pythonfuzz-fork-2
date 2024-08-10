07e921b610bc42a0b67fc528979cede640790207
Marcell Perger <marcell.perger.1@gmail.com>
2024-08-11 00:03:01 +0100

    fix: Fix pickling errors in tests

--- tests/test_crash.py
+++ tests/test_crash.py
@@ -6,13 +6,14 @@ from unittest.mock import patch
 import pythonfuzz
 
 
+def _fuzz(buf):
+    f = io.BytesIO(buf)
+    z = zipfile.ZipFile(f)
+    z.testzip()
+
+
 class TestFindCrash(unittest.TestCase):
     def test_find_crash(self):
-        def fuzz(buf):
-            f = io.BytesIO(buf)
-            z = zipfile.ZipFile(f)
-            z.testzip()
-
         with patch('logging.Logger.info') as mock:
-            pythonfuzz.fuzzer.Fuzzer(fuzz).start()
+            pythonfuzz.fuzzer.Fuzzer(_fuzz).start()
             self.assertTrue(mock.called_once)
