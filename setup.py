e44b46fab3f27be8be1aa112aa34ea64c75b7c34
Marcell Perger <marcell.perger.1@gmail.com>
2024-08-11 15:51:09 +0100

    fix: Exclude tests from setup.py
    Fixes https://gitlab.com/gitlab-org/security-products/analyzers/fuzzers/pythonfuzz/-/issues/4

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setuptools.setup(
     name="pythonfuzz",
     version="1.0.10",
     author="GitLab B.V.",
+    maintainer="Marcell Perger",
     description="Coverage-guided fuzz testing for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -21,5 +22,5 @@ setuptools.setup(
         "Topic :: Software Development :: Testing"
     ],
     python_requires='>=3.5.3',
-    packages=setuptools.find_packages('.', exclude=("examples",))
+    packages=setuptools.find_packages('.', exclude=("examples", "tests"))
 )
